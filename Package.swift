36a909e349f62794cc385ed801fdac4a229e59e6
laosb <git@lao.sb>
2026-04-16 13:15:56 +0800

    chore: bump deps

--- Package.swift
+++ Package.swift
@@ -25,12 +25,12 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/containerization.git", from: "0.29.0"),
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
-    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-nio.git", from: "2.70.0"),
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.23.0"),
+    .package(url: "https://github.com/apple/containerization.git", from: "0.30.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
+    .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0"),
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.12.0"),
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.97.0"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.33.1"),
   ],
   targets: [
     .target(
